b10024665023640a6313b843fc3a543ffa48638f
Kevin Kabore <kevin.s.kabore@gmail.com>
2016-06-22 17:34:01 -0700

    Updated to avoid recursion

--- main.js
+++ main.js
@@ -3,8 +3,20 @@ var wdi_24_names = ["Adrianna", "Andrew", "Christian", "Daniel", "David",
                     "Michael", "Ramin", "Taylor"];
 
 
-
+alreadyCalled = [];
 function eachName() {
+
   var randomName = wdi_24_names[Math.floor(Math.random() * wdi_24_names.length)];
-  console.log(randomName);
+  var index = alreadyCalled.indexOf(randomName);
+  if (index === -1){
+    console.log(randomName);
+    alreadyCalled.push(randomName);
+  } else {
+    eachName();
+  }
 }
+
+// var newArray = [];
+// var a=["a","b","c"];
+// for(var i=0;i<a.length;i++)
+//     if(a[i]!=="a") newArray.push(a[i]);
